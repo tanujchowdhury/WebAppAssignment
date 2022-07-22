a08a2472effa6090e5d63e9fd6370093e6c7e33d
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-21 19:06:49 -0700

    Updated html, css, and js

--- WebAppAssignment/wwwroot/js/site.js
+++ WebAppAssignment/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var id = null;
+function moveAnimation() {
+    var elem = document.getElementById("animation-element");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 1050) {
+            clearInterval(id);
+        }
+        else {
+            pos++;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
